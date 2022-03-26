b3fd0b1a813144734d96c6efd4bcc7892b9dec00
Pedro Parente <85114209+pedrop07@users.noreply.github.com>
2022-03-26 20:47:24 -0300

    refactor: changed styles and localStorage storage

--- main.js
+++ main.js
@@ -1,78 +1,70 @@
-window.onload = function() {
-    const list = document.querySelector('.wrapper');
-    const addButton = document.querySelector('.add_Button');
-    const items_area = document.querySelector('.items');
+const list = document.querySelector('.wrapper');
+const inputForm = document.getElementById('form');
 
-    addButton.onclick = function() {
-        const items = document.querySelector('.text-items').value;
-        let containers = document.createElement("div");
-        containers.classList.add('container_list');
+inputForm.addEventListener('submit', function(ev){
+    ev.preventDefault();
 
-        if(!items){
-            const wrap_div = document.querySelector('.wrap');
-            let error = document.createElement('p');
-            error.className = 'error-text';
-            error.innerHTML = '<i style="color: rgb(228, 52, 52)">Adicione um item a sua lista</i>'
+    let items = document.querySelector('.text-items');
+    let containers = document.createElement("div");
+    containers.classList.add('container_list');
 
-            for(error of document.querySelectorAll('.error-text')){
-                error.remove()
-            }
+    if(!items.value.trim()){
+        const wrap_div = document.querySelector('.wrap');
+        let error = document.createElement('p');
+        error.classList.add('error-text');
+        error.innerHTML = '<i style="color: rgb(228, 52, 52)">Adicione um item a sua lista</i>';
 
-            wrap_div.appendChild(error);
+        const presentError = document.querySelector('.error-text');
+        presentError?.remove();
+
+        wrap_div.appendChild(error);
+    } else {
+        let localItems = JSON.parse(localStorage.getItem('localItem'));
+
+        if(!localItems){
+            taskList = []
+        } else {
+            taskList = localItems;
         }
 
-        if(items) {
-            for(error of document.querySelectorAll('.error-text')){
-                error.remove()
-            }
+        taskList.push(items.value);
+        localStorage.setItem('localItem', JSON.stringify(taskList));
 
-            let del_button = document.createElement('button');
-            del_button.className = 'del_button';
-            del_button.innerHTML = 'Apagar';
+        const presentError = document.querySelector('.error-text');
+        presentError?.remove();
 
-            let edit_button = document.createElement("button");
-            edit_button.className = "edit_button";
-            edit_button.className = "edit_hover";
-
-            edit_button.innerHTML = 'Editar'
-
-            let div = document.createElement('div');
-            div.className = 'innerButtons'
-            div.appendChild(edit_button);
-            div.appendChild(del_button);
-
-            list.appendChild(containers);
-            containers.innerHTML = items;
-            containers.appendChild(div);
-
-            del_button.onclick = function() {
-                list.removeChild(containers)
-            }
-
-            edit_button.onclick = function() {
-                let input = document.createElement('input');
-                input.setAttribute("type", "text");
-                input.setAttribute("placeholder", "Adicione um item ");
-                input.className = 'input_edit';
-
-                edit_button.innerHTML = 'Confirmar'
-                containers.innerHTML = '';
-                containers.appendChild(input);
-                containers.appendChild(div);
-
-                edit_button.onclick = function() {
-                    if(!input.value){
-                        alert('Adicione um item !')
-                    } else {
-                        edit_button.innerHTML = "Editado";
-                        edit_button.classList.remove('edit_hover');
-                        edit_button.classList.add('edited');
-                        containers.innerHTML = input.value;
-                        containers.appendChild(div);
-                    }
-                }
-            }
-        }
+        showItem();
     }
+
+    items.value = '';
+})
+
+function showItem() {
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+    if(!localItems){
+        taskList = [];
+    } else {
+        taskList = localItems;
+    }
+
+    itemShow = document.querySelector('.wrapper');
+
+    let html = ''
+    taskList.forEach((item, index)=> {
+        html +=
+            `<div class="container_list">
+                ${item}
+                <button class="del-button" onClick="deleteItem(${index})">Apagar</button>
+            </div>`;
+    });
+
+    itemShow.innerHTML = html;
 }
 
+showItem();
+
+function deleteItem(index){
+    taskList.splice(index, 1);
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    showItem();
+}
